93122e7f1cb8dacf728f67a9079fd7d6d38912a8
JaeHyuk Choi <jaehyuk1220@gmail.com>
2022-03-23 03:44:32 +0900

    apply cosmos to search view

--- itunes-search/UI/ItunesAppSearch/CollectionViewCell/AppCollectionViewCell.swift
+++ itunes-search/UI/ItunesAppSearch/CollectionViewCell/AppCollectionViewCell.swift
@@ -6,12 +6,14 @@
 //
 import UIKit
 import Foundation
+import Cosmos
 
 class AppCollectionViewCell: UICollectionViewCell {
     @IBOutlet var appIcon: UIImageView!
     @IBOutlet var title: UILabel!
     @IBOutlet var subTitle: UILabel!
     @IBOutlet var priceButton: UIButton!
+    @IBOutlet var starRatingView: CosmosView!
     
     @IBOutlet var screenShots: [UIImageView]!
     func configureCell(appInfo: ITunesSearchAppViewModel) {
@@ -19,6 +21,8 @@ class AppCollectionViewCell: UICollectionViewCell {
         appIcon.load(from: appInfo.mainAppIconUrl)
         title.text = appInfo.title
         subTitle.text = appInfo.subTitle
+        starRatingView.rating = Double(appInfo.averageUserRating)
+        starRatingView.text = String(appInfo.userRatingCount)
         priceButton.titleLabel?.text = appInfo.price
         screenShots.forEach { screenShot in
             if appInfo.screenshotUrls.count > screenShot.tag {
